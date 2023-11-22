4ba2c37f89e03ea8e0d92a6f0ed1e916d7fdd14a
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-22 03:50:15 +0200

    Update def all(...) to return the list of objects of one type of class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,11 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            return [obj for obj in FileStorage.__objects.values()
+                    if isinstance(obj, cls)]
+        else:
+            return list(FileStorage.__objects.values())
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
