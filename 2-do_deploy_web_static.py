13606a16c37df6367c09a9cb9ddca3c68e287185
LwaziNcubeX <lwazincubex@gmail.com>
2023-12-08 02:57:12 +0200

    distributes an archive to your web servers.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Distributes an archive to the web servers."""
-from fabric.api import env, run, put
+"""distributes an archive to your web servers"""
 import os
+from fabric.api import env, run, put
 
 env.hosts = ['100.26.170.206', '34.202.159.66']
 env.user = 'ubuntu'
@@ -9,30 +9,51 @@ env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers."""
-    if not os.path.exists(archive_path):
-        print(f"Error: Archive '{archive_path}' not found.")
-        return False
-
+    """distributes an archive to your web servers"""
     try:
+        # Check if the archive file exists
+        if not os.path.exists(archive_path):
+            raise FileNotFoundError(
+                f"The archive file {archive_path} does not exist.")
+
+        # Extract necessary information from the archive path
+        file_name = archive_path.split("/")[-1]
+        archive_name = file_name.split(".")[0]
+        release_path = "/data/web_static/releases/"
+
+        # Upload the archive to the /tmp/ directory on the server
         put(archive_path, '/tmp/')
 
-        archive_filename = os.path.basename(archive_path)
-        release_folder = f'/data/web_static/releases/{archive_filename.split(".")[0]}'
-        run(f"mkdir -p {release_folder}")
-        run(f"tar -xzf /tmp/{archive_filename} -C {release_folder}")
+        # Create a new release directory
+        run('mkdir -p {}{}/'.format(release_path, archive_name))
 
-        # Remove the uploaded archive from the /tmp/ directory
-        run(f"rm /tmp/{archive_filename}")
+        # Extract the contents of the archive to the release directory
+        run('tar -xzf /tmp/{} -C {}{}/'.format(
+            file_name, release_path, archive_name))
 
-        # Create or update the symbolic link to the new release
-        current_link = '/data/web_static/current'
-        run(f"rm -f {current_link}")
-        run(f"ln -s {release_folder} {current_link}")
+        # Remove the uploaded archive from /tmp/
+        run('rm /tmp/{}'.format(file_name))
+
+        # Move the contents to the parent directory
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(
+            release_path, archive_name))
+
+        # Remove the web_static directory within the release directory
+        run('rm -rf {}{}/web_static'.format(release_path, archive_name))
+
+        # Remove the existing /data/web_static/current symbolic link
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link to the latest release
+        run('ln -s {}{}/ /data/web_static/current'.format(
+            release_path, archive_name))
 
         print("New version deployed successfully.")
         return True
 
-    except Exception as e:
+    except FileNotFoundError as e:
         print(f"Error: {e}")
+        return False
+    except Exception as e:
+        print(f"Error during deployment: {e}")
         return False
