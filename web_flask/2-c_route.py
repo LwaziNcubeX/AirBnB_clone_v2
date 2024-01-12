771904e9c166154914b92acbe0466b999d1e8bda
Lwazi Ncube <lwazincubex@gmail.com>
2024-01-12 16:16:14 +0200

    A script that returns A formatted Text.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """return hello HBNB message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return ..... HBNB message"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """display text after C Character"""
+    input_text = text.replace('_', ' ')
+
+    return f'C {input_text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
