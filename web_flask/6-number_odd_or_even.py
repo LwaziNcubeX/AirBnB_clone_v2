dd8bfb05a972963708b1bef398764d6d87b1e5e5
Lwazi Ncube <lwazincubex@gmail.com>
2024-01-23 16:31:05 +0200

    display a HTML page [odd or even]?

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """return hello HBNB message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return ..... HBNB message"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """display text after C Character"""
+    input_text = text.replace('_', ' ')
+
+    return f'C {input_text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is cool'):
+    """display text after python Character"""
+    input_text = text.replace('_', ' ')
+
+    return f'Python {input_text}'
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def display_num(n):
+    """display num else send error"""
+    try:
+        num = int(n)
+        return f'{num} is a number'
+    except ValueError:
+        abort(404)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', value=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    if n % 2 == 0:
+        state = "even"
+    else:
+        state = "odd"
+    return render_template('6-number_odd_or_even.html', n=n, state=state)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
