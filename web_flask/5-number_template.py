ea8ef1e7c1075e0a5b91712bf4bc5719892fbfbc
Lwazi Ncube <lwazincubex@gmail.com>
2024-01-23 16:12:05 +0200

    display a HTML page only if n is an integer

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """return hello HBNB message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return ..... HBNB message"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """display text after C Character"""
+    input_text = text.replace('_', ' ')
+
+    return f'C {input_text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is cool'):
+    """display text after python Character"""
+    input_text = text.replace('_', ' ')
+
+    return f'Python {input_text}'
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def display_num(n):
+    """display num else send error"""
+    try:
+        num = int(n)
+        return f'{num} is a number'
+    except ValueError:
+        abort(404)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
